a8db4eb3ec7291e0b4939a48c81fb2c394b7c4b2
Kadetys <ku4in2005@gmail.com>
2024-10-20 17:27:42 +0300

    .

--- Null and None/Null and None.c
+++ Null and None/Null and None.c
@@ -14,7 +14,7 @@ char igrok(char mas[3][4], int vybor) {
         vibor -= 1;
         if (mas[vibor / 3][vibor % 3] != '-') {
             printf("Ячейка занята, выберите другую!\n");
-            }
+        }
         if (vybor == 1) {
             mas[vibor / 3][vibor % 3] = 'X';
         }
@@ -23,35 +23,36 @@ char igrok(char mas[3][4], int vybor) {
         }
         break;
         return mas;
+    }
 }
 
-int main()
-{
-    int Choise = 0;
-    setlocale(LC_ALL, "");
-    printf("Нумерация клеток на поле: \n|-1-|-2-|-3-| \n|-4-|-5-|-6-| \n|-7-|-8-|-9-| \n");
-    while (1) {
-        printf("Совершите выбор: \n1: Крестик\n2: Нолик\n");
-        scanf("%d", &Choise);
-        if (Choise >2 || Choise < 1) {
-            printf("Ошибка, выберите один из предложенных пунктов\n");
+    int main()
+    {
+        int Choise = 0;
+        setlocale(LC_ALL, "");
+        printf("Нумерация клеток на поле: \n|-1-|-2-|-3-| \n|-4-|-5-|-6-| \n|-7-|-8-|-9-| \n");
+        while (1) {
+            printf("Совершите выбор: \n1: Крестик\n2: Нолик\n");
+            scanf("%d", &Choise);
+            if (Choise > 2 || Choise < 1) {
+                printf("Ошибка, выберите один из предложенных пунктов\n");
+            }
+            else {
+                break;
+            }
         }
-        else {
-            break;
+        char mas[3][4];
+        for (int i = 0; i != 3; i++) {
+            ;
+            for (int j = 0; j != 3; j++) {
+                mas[i][j] = '-';
+            }
+        }
+        mas[3][4] = bot(mas, Choise);
+        for (int i = 0; i != 3; i++) {
+            printf("\n");
+            for (int j = 0; j != 3; j++) {
+                printf("%c ", mas[i][j]);
+            }
         }
     }
-    char mas[3][4];
-    for (int i = 0; i != 3; i++) {
-        ;
-        for (int j = 0; j != 3; j++) {
-            mas[i][j] = '-';
-        }
-    }
-    mas[3][4] = bot(mas, Choise);
-    for (int i = 0; i != 3; i++) {
-        printf("\n");
-        for(int j = 0; j!= 3; j++){
-            printf("%c ", mas[i][j]);
-        }
-    }
-}
